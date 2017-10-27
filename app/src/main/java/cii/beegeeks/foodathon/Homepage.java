632bdd4691fcf2110f37b23e9a05e269c6faf56f
VivekHub97 <agarwalvivek.1997@gmail.com>
2017-10-28 00:12:52 +0530

    fragments added

--- app/src/main/java/cii/beegeeks/foodathon/Homepage.java
+++ app/src/main/java/cii/beegeeks/foodathon/Homepage.java
@@ -1,25 +1,13 @@
 package cii.beegeeks.foodathon;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.HeaderViewListAdapter;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.nightonke.boommenu.BoomButtons.HamButton;
+import com.nightonke.boommenu.BoomButtons.OnBMClickListener;
 import com.nightonke.boommenu.BoomMenuButton;
-import com.poliveira.parallaxrecyclerview.ParallaxRecyclerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Homepage extends AppCompatActivity {
 BoomMenuButton bmb;
@@ -32,27 +20,57 @@ BoomMenuButton bmb;
         HamButton.Builder builder1 = new HamButton.Builder()
                 .normalImageRes(R.drawable.salad)
                 .normalTextRes(R.string.menu_option1)
-                .subNormalTextRes(R.string.opt1_detail).normalColorRes(R.color.opt1);
+                .subNormalTextRes(R.string.opt1_detail).normalColorRes(R.color.opt1).listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home, new breakfast1()).commit();
+                    }
+                });
         bmb.addBuilder(builder1);
         HamButton.Builder builder4 = new HamButton.Builder()
                 .normalImageRes(R.drawable.salad)
                 .normalTextRes(R.string.menu_option4)
-                .subNormalTextRes(R.string.opt4_detail).normalColorRes(R.color.opt4);
+                .subNormalTextRes(R.string.opt4_detail).normalColorRes(R.color.opt4).listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home, new lunch1()).commit();
+                    }
+                });
         bmb.addBuilder(builder4);
         HamButton.Builder builder5 = new HamButton.Builder()
                 .normalImageRes(R.drawable.salad)
                 .normalTextRes(R.string.menu_option5)
-                .subNormalTextRes(R.string.opt5_detail).normalColorRes(R.color.opt5);
+                .subNormalTextRes(R.string.opt5_detail).normalColorRes(R.color.opt5).listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home, new dinner1()).commit();
+                    }
+                });
         bmb.addBuilder(builder5);
         HamButton.Builder builder3 = new HamButton.Builder()
                 .normalImageRes(R.drawable.cocktail)
                 .normalTextRes(R.string.menu_option3)
-                .subNormalTextRes(R.string.opt3_detail).normalColorRes(R.color.opt2);
+                .subNormalTextRes(R.string.opt3_detail).normalColorRes(R.color.opt2).listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home, new snacks1()).commit();
+                    }
+                });
         bmb.addBuilder(builder3);
         HamButton.Builder builder2 = new HamButton.Builder()
                 .normalImageRes(R.drawable.run)
                 .normalTextRes(R.string.menu_option2)
-                .subNormalTextRes(R.string.opt2_detail).normalColorRes(R.color.opt3);
+                .subNormalTextRes(R.string.opt2_detail).normalColorRes(R.color.opt3).listener(new OnBMClickListener() {
+                    @Override
+                    public void onBoomButtonClick(int index) {
+
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home, new excercise1()).commit();
+                    }
+                });
         bmb.addBuilder(builder2);
 
         //Bottom navigation code
@@ -67,10 +85,36 @@ BoomMenuButton bmb;
         bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
             @Override
             public void onTabSelected(int position) {
+                mytrack m1=new mytrack();
+                getSupportFragmentManager().beginTransaction().replace(R.id.home,m1).commit();
+                switch(position)
+                {
+                    case 0:
+                    {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home,m1).commit(); break;
+                    }
+                    case 1:
+                    {
+                        Alternative m2=new Alternative();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home,m2).commit(); break;
+                    }
+                    case 2:
+                    {
+                        Health m3=new Health();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home,m3).commit(); break;
+                    }
+                    case 4:
+                    {
+                        Profile m4=new Profile();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.home,m4).commit(); break;
+                    }
+
+                }
             }
 
             @Override
             public void onTabUnselected(int position) {
+
             }
 
             @Override
@@ -78,8 +122,5 @@ BoomMenuButton bmb;
             }
         });
 
-
-
     }
-
 }
